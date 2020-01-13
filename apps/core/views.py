cbc55da7ed8743594f7f411d98317702307eee21
gelhen <gelhen@gmail.com>
2020-01-12 22:15:31 -0300

    Vinculando Funcionario Logado

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from apps.funcionarios.models import Funcionario
+
 
 @login_required
 def home(request):
-    return render(request, 'core/index.html')
+    usuario = request.user
+    data = dict(usuario=usuario)
+    return render(request, 'core/index.html', data)
--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from apps.empresas.models import Empresa
 
-# Create your views here.
+
+class EmpresaCreate(CreateView):
+    model = Empresa
+    fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+
+        return HttpResponse('Ok')
