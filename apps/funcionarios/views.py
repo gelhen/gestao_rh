6973d8b9d7630e70465ce0b58486382fc5d240d2
gelhen <gelhen@gmail.com>
2020-01-16 23:38:42 -0300

    Filtrando funcionario pela empresa logada

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -4,4 +4,8 @@ from apps.funcionarios.models import Funcionario
 
 class FuncionariosList(ListView):
     model = Funcionario
-    paginate_by = 10
+    # paginate_by = 10
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
