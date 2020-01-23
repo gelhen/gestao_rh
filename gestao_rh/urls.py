9d688742138bf981debbeae8f0caed87c7333feb
gelhen <gelhen@gmail.com>
2020-01-22 22:20:06 -0300

    Crud Documentos

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,6 +9,7 @@ urlpatterns = [
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
     path('empresa/', include('apps.empresas.urls')),
+    path('documento/', include('apps.documentos.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #PARA VER ARQUIVOS DE MIDIA EM DEV
