5ba82c3a28bab4e10b25795d8db8ecb2cb851383
gelhen <gelhen@gmail.com>
2020-01-30 22:28:00 -0300

    Implementado exclusão do registro de hora extra

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from apps.registro_hora_extra.views import HoraExtraList, HoraExtraEdit
+from apps.registro_hora_extra.views import HoraExtraList, HoraExtraEdit, HoraExtraDelete
 
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
     # path('novo/', FuncionarioCreate.as_view(), name='create_funcionario'),
     path('editar/<int:pk>/', HoraExtraEdit.as_view(), name='update_hora_extra'),
-    # path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
+    path('delete/<int:pk>/', HoraExtraDelete.as_view(), name='delete_hora_extra'),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView
 
 from apps.registro_hora_extra.models import Registro_hora_extra
 
@@ -16,3 +17,7 @@ class HoraExtraEdit(UpdateView):
     model = Registro_hora_extra
     fields = ['motivo', 'funcionario', 'horas']
 
+
+class HoraExtraDelete(DeleteView):
+    model = Registro_hora_extra
+    success_url = reverse_lazy('list_hora_extra')
