24988ada8a13fb4e572079dc0fd2928ad514135c
gelhen <gelhen@gmail.com>
2020-02-27 08:11:42 -0300

    Implementado acesso via Token

--- apps/registro_hora_extra/api/views.py
+++ apps/registro_hora_extra/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from apps.registro_hora_extra.api.serializers import Registro_hora_extraSerializer
 from apps.registro_hora_extra.models import Registro_hora_extra
 
@@ -6,3 +8,5 @@ from apps.registro_hora_extra.models import Registro_hora_extra
 class Registro_hora_extraViewSet(viewsets.ModelViewSet):
     queryset = Registro_hora_extra.objects.all()
     serializer_class = Registro_hora_extraSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
