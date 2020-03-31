4b21e5a17f7974c08558b93f9ea0fc2072a7429f
gelhen <gelhen@gmail.com>
2020-03-30 22:34:03 -0300

    Dashboard finalizado

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,16 +1,29 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import render
 from apps.funcionarios.models import Funcionario
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from apps.core.serializers import UserSerializer, GroupSerializer
+from apps.registro_hora_extra.models import Registro_hora_extra
 from .tasks import send_relatorio
 
 @login_required
 def home(request):
-    usuario = request.user
-    data = dict(usuario=usuario)
+    data = {}
+    data['usuario'] = request.user
+    funcionario = request.user.funcionario
+    data['total_funcionarios'] = funcionario.empresa.total_funcionarios
+    data['total_funcionarios_ferias'] = funcionario.empresa.total_funcionarios_ferias
+    data['total_funcionarios_doc_pendentes'] = funcionario.empresa.total_funcionarios_doc_pendente
+    data['total_funcionarios_doc_ok'] = funcionario.empresa.total_funcionarios_doc_ok
+    data['total_funcionarios_rg'] = 10
+    data['total_hora_extra_utilizadas'] = Registro_hora_extra.objects.filter(
+        funcionario__empresa=funcionario.empresa, utilizada=True).aggregate(Sum('horas'))['horas__sum']
+    data['total_hora_extra_pendente'] = Registro_hora_extra.objects.filter(
+        funcionario__empresa=funcionario.empresa, utilizada=False).aggregate(Sum('horas'))['horas__sum']
+
     return render(request, 'core/index.html', data)
 
 
