d43bc40fbc2d6d59f9ba5618df537074b583bc67
gelhen <gelhen@gmail.com>
2020-03-31 22:39:57 -0300

    Filtrando opções de um select com Ajax

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from apps.departamentos.models import Departamento
 from apps.funcionarios.models import Funcionario
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
@@ -31,6 +34,16 @@ def celery(request):
     send_relatorio.delay()
     return HttpResponse('Tarefa incluída na fila para execução')
 
+def departamentos_ajax(request):
+    departamentos = Departamento.objects.all()
+    return render(request, 'departamentos_ajax.html', {'departamentos':departamentos})
+
+def filtra_funcionarios(request):
+    depart = request.GET['outro_param']
+    departamento = Departamento.objects.get(id=depart)
+
+    qs_json = serializers.serialize('json', departamento.funcionario_set.all())
+    return HttpResponse(qs_json, content_type='application/json')
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
--- staticfiles/js/funcoes-ajax.js
+++ staticfiles/js/funcoes-ajax.js
@@ -20,3 +20,29 @@ function utilizouHoraExtra(id, utilizou){
         }
     });
 }
+
+function process_response(funcionarios){
+    func_select = document.getElementById('funcionarios');
+    func_select.innerHTML = '';
+
+    funcionarios.forEach(function(funcionario){
+        var option = document.createElement("option");
+        option.text = funcionario.fields.nome;
+        func_select.add(option)
+    });
+}
+
+function filtraFuncionarios(){
+    depart_id = document.getElementById('departamentos').value;
+    $.ajax({
+        type: 'GET',
+        url: '/filtra-funcionarios/',
+        data: {
+            outro_param: depart_id
+        },
+        success: function(result){
+            process_response(result);
+            $("#mensagem").text('Funcionarios carregados');
+        }
+    });
+}
