3e7b933ec446320967f877c7b2f27f9fd43c3879
gelhen <gelhen@gmail.com>
2020-06-25 10:57:04 -0300

    alteração do site do admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+
+admin.site.site_header = 'Administração Gestão RH'
+admin.site.site_title = 'Gestão RH'
+admin.site.index_title = 'Administração Gestão RH'
